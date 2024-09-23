49fee50421cd64e223597f017cf000d976685c39
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2024-09-23 20:04:14 +0530

    Add files via upload

---
+++ K6.java
@@ -0,0 +1,35 @@
+/*variable declaration and initialization*/
+public class K6{
+	public static void main(String[] args){
+	int myInt;
+      myInt=85;
+	System.out.println(myInt);
+
+      long myNumber=779999999;
+      System.out.println(myNumber);
+
+	float myFloat=95.9f;
+	System.out.println(myFloat);
+
+	double myDouble=98.09887898;
+	System.out.println(myDouble);
+
+	boolean isVegitarian=true;
+	System.out.println(isVegitarian);
+
+	String myThing="good morning";
+	System.out.println(myThing);
+
+	char myChar='p';
+	System.out.println(myChar);
+/*output
+85
+779999999
+95.9
+98.09887898
+true
+good morning
+p */
+
+	}
+}
