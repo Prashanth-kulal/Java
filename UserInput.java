0e456288c115e4092056725858e0d47ddfa693c7
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2024-09-24 20:17:37 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class UserInput{
 public static void main(String[] args){
-	System.out.print("Please enter your name: ");
 	Scanner input=new Scanner(System.in);
+	System.out.print("Please enter your name: ");
 	String name=input.nextLine();
 	System.out.println("Good morning " + name);
 
