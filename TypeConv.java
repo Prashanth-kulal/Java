85099286678cf05a7108827f4f2d8370c18499cd
Prashanth-kulal <prashanth.23cs098@sode-edu.in>
2024-09-25 20:01:50 +0530

    Add files via upload

---
+++ TypeConv.java
@@ -0,0 +1,10 @@
+/*program for Type conversion*/
+ public class TypeConv{
+    public static void main(String[] args){
+        float n1=5;
+        System.out.println(n1);
+        int n2=(int)5.0;
+        System.out.println(n2);
+        
+    }
+ }
